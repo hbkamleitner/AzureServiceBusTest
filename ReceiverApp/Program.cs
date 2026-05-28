bc6f966ab6dda63f5825490ba3dbf5d62d4b31ff
Bernhard Kamleitner <bernhard.kamleitner@hagleitner.com>
2026-05-28 14:58:14 +0200

    add recevier app

---
+++ ReceiverApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World Receiver App!");
+
+// handle received messages
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+
+    // complete the message. message is deleted from the queue. 
+    await args.CompleteMessageAsync(args.Message);
+}
+
+// handle any errors when receiving messages
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("app.dev.json")
+    .Build();
+
+
+var clientOptions = new ServiceBusClientOptions
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+};  
+
+await using var client = new ServiceBusClient(
+    configuration["ServiceBus:Namespace"],
+    new ClientSecretCredential(
+        configuration["AzureAd:TenantId"],
+        configuration["AzureAd:ClientId"],
+        configuration["AzureAd:ClientSecret"]
+    ),
+    clientOptions
+);
+
+try
+{
+    await using var processor = client.CreateProcessor(configuration["ServiceBus:QueueName"]);
+
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler; 
+    
+    Console.WriteLine("Message received successfully.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error receiving message: {ex.Message}");
+}
+
+
--- SenderApp/Program.cs
+++ SenderApp/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("app.dev.json")
+    .Build();
+
+
+var clientOptions = new ServiceBusClientOptions
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+};  
+
+await using var client = new ServiceBusClient(
+    configuration["ServiceBus:Namespace"],
+    new ClientSecretCredential(
+        configuration["AzureAd:TenantId"],
+        configuration["AzureAd:ClientId"],
+        configuration["AzureAd:ClientSecret"]
+    ),
+    clientOptions
+);
+
+try
+{
+    await using var sender = client.CreateSender(configuration["ServiceBus:QueueName"]);
+
+    var message = new ServiceBusMessage($"Hello again, Service Bus! {DateTime.Now}");
+
+    await sender.SendMessageAsync(message);
+
+    Console.WriteLine("Message sent successfully.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error sending message: {ex.Message}");
+}
+
+
